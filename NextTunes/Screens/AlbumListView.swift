8affb9c913c52b932b4c1c32abec2bf4dbcb311d
Diego Llopis <diego.llopis1@gmail.com>
2021-09-17 22:01:34 -0300

    Correção bug navigation bar buttons

--- NextTunes/Screens/AlbumListView.swift
+++ NextTunes/Screens/AlbumListView.swift
@@ -12,6 +12,8 @@ struct AlbumListView: View {
     let networkClient: NetworkClient
     
     @StateObject var albumListViewModel = AlbumListViewModel()
+    
+    @State private var editMode = EditMode.inactive
     @State private var showingSearch = false
     
     var body: some View {
@@ -25,38 +27,41 @@ struct AlbumListView: View {
                 .onDelete(perform: albumListViewModel.deleteAlbum)
                 .onMove(perform: albumListViewModel.moveAlbum)
             }
-            
-            .toolbar {
-                
-                HStack {
-                    
-                    EditButton()
-                    
-                    Button("Search") {
-                        
-                        showingSearch = true
-                    }
-                    .sheet(isPresented: $showingSearch,
-                           onDismiss: {
-                            
-                            if let selectedAlbum = albumListViewModel.selectedAlbum {
-                                
-                                albumListViewModel.albums.append(selectedAlbum)
-                                albumListViewModel.selectedAlbum = nil
-                            }
-                           }) {
-                        
-                        AlbumSearchView(showModal: $showingSearch, onAlbumSelected: { album in
-                            
-                            albumListViewModel.selectedAlbum = album
-                        })
-                }
-                }
-            }
+            .environment(\.editMode, $editMode)
+            .animation(.spring(response: 0))
+            .listStyle(PlainListStyle())
             .navigationTitle("Next Tunes")
+            .navigationBarItems(
+                trailing:
+                    
+                    HStack {
+                        
+                       EditButton(editMode: $editMode)
+                        
+                        Button("Search") {
+                            
+                            editMode = EditMode.inactive
+                            showingSearch = true
+                        }
+                        .sheet(isPresented: $showingSearch,
+                               onDismiss: {
+                                
+                                if let selectedAlbum = albumListViewModel.selectedAlbum {
+                                    
+                                    albumListViewModel.albums.append(selectedAlbum)
+                                    albumListViewModel.selectedAlbum = nil
+                                }
+                               }) {
+                            
+                            AlbumSearchView(showModal: $showingSearch, onAlbumSelected: { album in
+                                
+                                albumListViewModel.selectedAlbum = album
+                            })
+                    }
+                })
+            }
         }
     }
-}
 
 struct AlbumListView_Previews: PreviewProvider {
     
---
+++ NextTunes/Screens/Components/EditButton.swift
@@ -0,0 +1,30 @@
+//
+//  CustomEditButtonView.swift
+//  NextTunes
+//
+//  Created by Diego Llopis on 17/09/21.
+//
+
+import SwiftUI
+
+struct EditButton: View {
+    
+    @Binding var editMode: EditMode
+
+    var body: some View {
+        
+        Button {
+            switch editMode {
+            case .active: editMode = .inactive
+            case .inactive: editMode = .active
+            default: break
+            }
+        } label: {
+            if let isEditing = editMode.isEditing, isEditing {
+                Text("Done")
+            } else {
+                Text("Edit")
+            }
+        }
+    }
+}
