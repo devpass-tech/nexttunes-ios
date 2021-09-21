b6de2c9cdf5a69dcc5454aada4e8da2aafdf285a
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2021-09-21 16:55:46 -0300

    Adicionado formate date e unit tests

---
+++ NextTunes/Extensions/FormatterDate+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  FormatterDate+Extension.swift
+//  NextTunes
+//
+//  Created by Alexandre Cardoso on 21/09/21.
+//
+
+import Foundation
+
+extension String {
+	
+	func formatterDate() -> String? {
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateFormat = "yyyy-MM-dd"
+		
+		guard let dateFormatterInput = dateFormatter.date(from: self)
+		else { return nil }
+		dateFormatter.dateFormat = "dd/MM/yyyy"
+		
+		return dateFormatter.string(from: dateFormatterInput)
+	}
+	
+}
