12b82eccd4c2551dcf19cf137ece05dcb7854204
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2021-09-22 19:21:52 -0300

    ajuste nomenclatura metodo

--- NextTunesTests/FormatterDate+ExtensionTests.swift
+++ NextTunesTests/FormatterDate+ExtensionTests.swift
@@ -10,13 +10,13 @@ import XCTest
 
 class FormatterDate_ExtensionTests: XCTestCase {
 	
-	func test_wrongDateFormatShouldReturnNil() {
-		let sut: String? = "21.09.2021"
+	func test_DateFormat_whenWrongDate_shouldReturnNil() {
+		let sut: String? = "21.09-202"
 		
 		XCTAssertNil(sut?.formatterDate())
 	}
 	
-	func test_DateFormatShouldReturnString() {
+	func test_DateFormatter_whenCorrectlyDate_shouldReturnStringSuccess() {
 		let sut: String? = "2021-09-21"
 		let expected: String? = "21/09/2021"
 		
