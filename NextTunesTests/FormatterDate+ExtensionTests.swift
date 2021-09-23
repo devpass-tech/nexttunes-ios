ef290b90a94a3f788a8db61692053763ff71518c
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2021-09-23 20:08:46 -0300

    Implementado regra de formatar data e tests

---
+++ NextTunesTests/FormatterDate+ExtensionTests.swift
@@ -0,0 +1,28 @@
+//
+//  FormatterDate+ExtensionTests.swift
+//  NextTunesTests
+//
+//  Created by Alexandre Cardoso on 23/09/21.
+//
+
+import XCTest
+@testable import NextTunes
+
+
+class FormatterDate_ExtensionTests: XCTestCase {
+	
+	func test_wrongDateFormatShouldReturnNil() {
+		let sut: String? = "21.09.2021"
+		
+		XCTAssertNil(sut?.formatterDate())
+	}
+	
+	func test_DateFormatShouldReturnString() {
+		let sut: String? = "2021-09-21"
+		let expected: String? = "21/09/2021"
+		
+		XCTAssertNotNil(sut?.formatterDate())
+		XCTAssertTrue(sut?.formatterDate() == expected)
+	}
+	
+}
